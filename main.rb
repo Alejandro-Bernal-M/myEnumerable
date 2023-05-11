f1452e86be354aaa5bb4a725bec556d85aa97231
alejandro <alejandrober96@gmail.com>
2023-05-11 15:00:53 -0500

    Add main document where the class MyList is defined

---
+++ main.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Import my_enumerable.rb
+
+require_relative 'my_enumerable'
+
+# Create MyList class
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each { |n| block.call(n) }
+  end
+end
